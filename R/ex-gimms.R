55b3e67cfbc24819ef4799ca68c366b377c6250b
fdetsch <fdetsch@web.de>
2020-09-22 09:58:30 +0200

    added visualization to example.

--- R/ex-gimms.R
+++ R/ex-gimms.R
@@ -1,4 +1,20 @@
 library(gimms)
+library(latticeExtra)
 
-onl = updateInventory()
+onl = updateInventory(server = "ecocast")
 head(onl)
+
+dts = monthlyIndices(onl, timestamp = TRUE)
+cnt = aggregate(dts, by = list("year" = format(dts, "%Y")), FUN = length)
+
+barchart(
+  x ~ as.integer(year)
+  , data = cnt
+  , horizontal = FALSE
+  , col = "grey"
+  , panel = function(x, y, ...) {
+    panel.barchart(x, y, ...)
+    panel.ablineq(lm(y ~ x), rotate = TRUE, col.line = "red", lty = 2)
+  }
+  , xlab = "year", ylab = "# files"
+)
